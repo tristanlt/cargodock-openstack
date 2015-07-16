510d1933876111a6cef4a1bfd85b58442e78d13b
Ubuntu <ubuntu@cargodock-os-1.novalocal>
2015-07-16 09:48:22 +0000

    First working version

---
+++ http-envvar.sh
@@ -0,0 +1,2 @@
+export KV3="http://localhost:5000/v3/"
+export TOK="X-Auth-Token: ADMIN"
---
+++ osc-envvars.sh
@@ -0,0 +1,8 @@
+export OS_USERNAME=cloudadmin
+export OS_PROJECT_NAME=cloudadmin
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PASSWORD=cloudadmin
+export OS_AUTH_URL=http://9lpo133:5000/v3
+export OS_URL=http://9lpo133:5000/v3
+export OS_IDENTITY_API_VERSION=3
